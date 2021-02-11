d93ef8b0d2009e6b42917a74fe27665e8fee9c1c
dmitrypavlikov <dmitriipavlikov@gmail.com>
2021-02-11 19:19:10 +0300

    Homework 11.02 until lesson

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,40 @@ import Shapes.Round;
 import Shapes.Triangle;
 import myArray.myArray;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println();
         Scanner scanner = new Scanner(System.in);
-       try {
-           int n = Integer.parseInt(scanner.nextLine());
-       } catch (Exception e) {
-           System.out.println("Несоответствие данных!");
-       }
-       String value = String.valueOf(scanner.nextLine());
-       ArrayList mass = new ArrayList<String>(Integer.parseInt(value));
-       for(int i = 0; i < value.length(); i++){
-               if(value.charAt(i) != ' '){
-                 try{
-                     mass.add((int) value.charAt(i));
-                 } catch (Exception e){
-                     System.out.println("Несоответствие данных!");
-                 }
+        Integer n = scanner.nextInt();
+        ArrayList<Integer> array = new ArrayList<>();
+        ArrayList<Integer> arrayMask = new ArrayList<>();
+        TreeSet<Integer> multiple = new TreeSet<>();
+        for (int i = 0; i < n; i++){
+            array.add(scanner.nextInt());
+            arrayMask.add(array.get(i));
+            multiple.add(array.get(i));
+        }
+        int min = multiple.first();
+        int max = multiple.last();
+        System.out.printf("Используются только первые " + n);
+        if (multiple.size() > 2){
+            System.exit(-1);
+        }
+        Collections.sort(arrayMask);
+        for (int i = 0; i < n; i++){
+            if(array.get(i) != arrayMask.get(i)){
+                System.exit(-1);
+            }
+        }
+        System.exit(max-min);
 
-               }
 
 
-       }
-       for(Integer k : mass ){
-           System.out.println(k);
-       }
+
+
+
     }
 
 
