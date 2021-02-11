41fff4afe888f1a66ce58bb06d678f9ba7532b36
dmitrypavlikov <dmitriipavlikov@gmail.com>
2021-02-12 00:28:41 +0300

    Homework 11.02 after lesson

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,11 @@ import Shapes.Parallelogram;
 import Shapes.Round;
 import Shapes.Triangle;
 import myArray.myArray;
-
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        /*
         Scanner scanner = new Scanner(System.in);
         Integer n = scanner.nextInt();
         ArrayList<Integer> array = new ArrayList<>();
@@ -31,6 +30,58 @@ public class Main {
             }
         }
         System.exit(max-min);
+        */
+        String data;
+        Scanner scanner = new Scanner(System.in);
+        data = scanner.nextLine();
+        String[] mass = data.split(" ");
+        if (mass.length != 3){
+            System.out.printf("Error");
+            System.exit(404);
+        }
+        else if ((Integer.valueOf(mass[1]) > 12 ) || (Integer.valueOf(mass[1]) <= 0)  && (Integer.valueOf(mass[2]) > 0)){
+            System.out.printf("Error");
+            System.exit(404);
+        }
+        else if (Integer.valueOf(mass[1]) == 2) {
+            if ((Integer.valueOf(mass[2]) % 4 == 0) || (Integer.valueOf(mass[2]) % 400 == 0) && (Integer.valueOf(mass[2]) % 100 != 0)) {
+                if ((Integer.valueOf(mass[0]) <= 29) && (Integer.valueOf(mass[0]) >= 1)){
+                    System.out.printf("Exist");
+                    System.exit(0);
+                }
+                else{
+                    System.out.printf("Error");
+                    System.exit(404);
+                }
+            }
+            else if ((Integer.valueOf(mass[0]) <= 28) && (Integer.valueOf(mass[0]) >= 1)){
+                System.out.printf("Exist");
+                System.exit(0);
+            }
+            else{
+                System.out.printf("Error");
+                System.exit(404);
+            }
+        }
+        else if(((Integer.valueOf(mass[1]) % 2 == 0) && (Integer.valueOf(mass[1]) <= 7)) || ((Integer.valueOf(mass[1]) % 2 != 0) && (Integer.valueOf(mass[1]) >= 8)))   {
+            if ((Integer.valueOf(mass[0]) <= 30) && (Integer.valueOf(mass[0]) >= 1)){
+                System.out.printf("Exist");
+                System.exit(0);
+            }
+            else{
+                System.out.printf("Error");
+                System.exit(404);
+            }
+
+        }
+        else if((Integer.valueOf(mass[0]) <= 31) && (Integer.valueOf(mass[0]) >= 1)){
+            System.out.printf("Exist");
+            System.exit(0);
+        }
+        else{
+            System.out.printf("Error");
+            System.exit(404);
+        }
 
 
 
